cde72b4a9952453924ef179db6f8a3df445af7ae
Josh <rueck.joshua@gmail.com>
2025-02-22 12:21:57 +0100

    Initial Commit

---
+++ bookshelfapp/bookshelfappApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookshelfappApp.swift
+//  bookshelfapp
+//
+//  Created by Joshua Rück on 22.02.25.
+//
+
+import SwiftUI
+
+@main
+struct bookshelfappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bookshelfappTests/bookshelfappTests.swift
@@ -0,0 +1,17 @@
+//
+//  bookshelfappTests.swift
+//  bookshelfappTests
+//
+//  Created by Joshua Rück on 22.02.25.
+//
+
+import Testing
+@testable import bookshelfapp
+
+struct bookshelfappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
